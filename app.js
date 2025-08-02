5be0ba60fa2cc92b02901cbe96ee4fe2aba1862e
Ignatius_Reilly <dmitriy_xix@hotmail.com>
2025-08-02 03:02:50 -0400

    taking a break

---
+++ app.js
@@ -0,0 +1,60 @@
+const todoForm = document.querySelector('form');
+const todoInput = document.getElementById('todo-input');
+const todoListUL = document.getElementById('todo-list');
+
+let allTodos = getTodos();
+updateTodoList();
+
+todoForm.addEventListener('submit', (e)=>{
+    e.preventDefault(); // this stops automatic reloading of page.
+    addTodo();
+})
+
+function addTodo(){
+    const todoText = todoInput.value.trim();
+    if(todoText.length > 0){
+        allTodos.push(todoText);
+        updateTodoList();
+        saveTodos();
+        todoInput.value="";
+    }
+}
+
+function updateTodoList(){
+    todoListUL.innerHTML="";
+    allTodos.forEach((todo, todoIndex)=>{ // this function will execute once for every element in the array.
+        todoItem = createTodoItem(todo,todoIndex);
+        todoListUL.append(todoItem);
+    })
+}
+
+function createTodoItem(todo,todoIndex){
+    const todoID = "todo-"+todoIndex;
+    const todoLI = document.createElement("li");
+    todoLI.className="todo";
+    todoLI.innerHTML = `
+                <input type="checkbox" id="${todoID}">
+                <label for="${todoID}" class="custom-checkbox">
+                    <svg fill="transparent" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="m381-240 424-424-57-56-368 367-169-170-57 57 227 226Zm0 113L42-466l169-170 170 170 366-367 172 168-538 538Z"/></svg>
+                </label>
+                <label for="${todoID}" class="todo-text">
+                    ${todo}
+                </label>
+                <button class="delete-button">
+                    <svg fill="var(--secondary-color)" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+                </button>
+    `;
+    return todoLI;
+}
+
+function saveTodos() {
+    const todosJSON = JSON.stringify(allTodos);
+    alert(todosJSON);
+    localStorage.setItem("todos",todosJSON);
+}
+
+function getTodos() {
+    const todos = localStorage.getItem("todos") || "[]";    
+    console.log(JSON.parse(todos));
+    return JSON.parse(todos);
+}
